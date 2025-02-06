03ae57266b50f6665f42c17a2a54e445b2f9972f
Barbaros Uzunlar <barbarosuzunlar2003@gmail.com>
2025-02-06 22:36:44 +0300

    3

--- games/fight-game/index.js
+++ games/fight-game/index.js
@@ -19,7 +19,7 @@ class Sprite {
         this.position = position
         this.velocity = velocity
         this.height = 150
-        this.width = 60
+        this.width = 70
         this.lastKey
         this.attackBox = {
             position:this.position,
